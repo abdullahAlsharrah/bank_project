5b30ad799a20286c1c90132cc823568fb1d32c80
calipho <106757959+calipho@users.noreply.github.com>
2022-07-03 07:24:10 +0300

    refresh button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:bank_project/providers/auth_providers.dart';
+import 'package:bank_project/providers/trans_providers.dart';
 import 'package:bank_project/widgets/actions.dart';
 import 'package:bank_project/widgets/curren_balance.dart';
 import 'package:bank_project/widgets/drawer.dart';
@@ -39,61 +40,70 @@ class _HomePageState extends State<HomePage> {
           body: Padding(
             padding: const EdgeInsets.only(top: 12.0, left: 40, right: 40),
             child: SafeArea(
-                child: SingleChildScrollView(
-              physics: const ScrollPhysics(),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(100),
-                            child: SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: InkWell(
-                                  onTap: () =>
-                                      {_sKey.currentState?.openDrawer()},
-                                  child: Image.network(
-                                    auth.user?.image ??
-                                        "https://media.istockphoto.com/vectors/user-avatar-profile-icon-black-vector-illustration-vector-id1209654046?s=612x612",
-                                    fit: BoxFit.fitHeight,
-                                  )),
+                child: RefreshIndicator(
+              onRefresh: () async {
+                await Future.delayed(Duration(seconds: 2));
+                updateData();
+              },
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: SizedBox(
+                                width: 50,
+                                height: 50,
+                                child: InkWell(
+                                    onTap: () =>
+                                        {_sKey.currentState?.openDrawer()},
+                                    child: Image.network(
+                                      auth.user?.image ??
+                                          "https://media.istockphoto.com/vectors/user-avatar-profile-icon-black-vector-illustration-vector-id1209654046?s=612x612",
+                                      fit: BoxFit.fitHeight,
+                                    )),
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  auth.user?.username ?? "sss",
-                                  style: const TextStyle(fontSize: 13),
-                                ),
-                                const Text(
-                                  "Welcome Back!",
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      const Icon(Icons.notifications_none)
-                    ],
-                  ),
-                  const CurrenBalance(),
-                  const ActionButtons(),
-                  RecentTransactions(list: "recenet", filter: "all")
-                ],
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    auth.user?.username ?? "sss",
+                                    style: const TextStyle(fontSize: 13),
+                                  ),
+                                  const Text(
+                                    "Welcome Back!",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        const Icon(Icons.notifications_none)
+                      ],
+                    ),
+                    const CurrenBalance(),
+                    const ActionButtons(),
+                    RecentTransactions(list: "recenet", filter: "all")
+                  ],
+                ),
               ),
             )),
           ));
     });
+  }
+
+  void updateData() {
+    context.read<TransProviders>().getTransactions();
   }
 }
